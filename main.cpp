bf47742c9ffd5dcb441e0a96005ea8023ad38e78
Nguyen Thanh Du <nguyentdu.work@gmail.com>
2026-03-13 10:45:15 -0700

    "Feat: add header(type) & initial version malloc

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+typedef char ALIGN[16];
+
+union header {
+    struct {
+        size_t size;
+        bool is_free;
+        header *next;
+    } s;
+    ALIGN stub;
+}
+
+typedef union header header_t;
+
+header_t *head, *tail;
+
+pthread_mutex_t global_malloc_lock;
+
+void *malloc(size_t size) {
+    void *block;
+    block = sbrk(size);
+    if (block == (*void) - 1)
+        return NULL;
+    return block;
+}
 
 
 int main() {
