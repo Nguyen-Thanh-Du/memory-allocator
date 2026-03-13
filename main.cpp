309eef36833c3744104fdf0e520a7d22ff50b498
Nguyen Thanh Du <nguyentdu.work@gmail.com>
2026-03-13 11:19:33 -0700

    Feat: Modify malloc() & Add free()

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 typedef char ALIGN[16];
 
+/*
+    size: To keep track of that memory block's size - used for free()
+    is_free: Differentiate free vs. release-able blocks;
+    stub: To align the header to 16-byte
+*/
 union header {
     struct {
         size_t size;
         bool is_free;
         header *next;
     } s;
-    ALIGN stub;
+    ALIGN stub; 
 }
 
 typedef union header header_t;
@@ -17,12 +22,81 @@ pthread_mutex_t global_malloc_lock;
 
 void *malloc(size_t size) {
     void *block;
-    block = sbrk(size);
-    if (block == (*void) - 1)
+    size_t total_size;
+    header_t *header;
+
+    if (!size)
         return NULL;
-    return block;
+    pthread_mutex_lock(&global_malloc_lock);
+    header = get_free_block(size);
+    if (header) {
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void*)(header + 1);
+    }
+    
+    total_size = sizeof(header_t) + size;
+    block = sbrk(total_size);
+    if (block == (void*) -1) {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+    header = block;
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+    if (!head)
+        head = header;
+    if (tail)
+        tail->s.next = header;
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+    return (void*)(header + 1);
 }
 
+header_t *get_free_block(size_t size) {
+    header_t *curr = head;
+    while (curr) {
+        if (curr->s.size >= size && curr->s.is_free == 1)
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
+}
+
+void free(void *block) {
+    header_t *header, *tmp;
+	void *programbreak;
+
+    if (!block)
+        return;
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (header_t*)(block) - 1;
+    programbreak = sbrk(0);
+    if (programbreak == (void*) - 1)
+        return;
+
+    if ((char*)block + header->s.size() == programmbreak) {
+        if (head == tail) {
+            head = tail == NULL;
+        } else {
+            tmp = head;
+            while (tmp) {
+                if (tmp->s.next == tail) {
+                    tail->s.next = NULL;
+                    tail = tmp;
+                }
+                tmp = tmp->s.next;
+            }
+            sbrk(0 - sizeof(header_t) - header->s.size);
+            pthread_mutex_unlock(&global_malloc_lock);
+            return;
+        }
+    }
+
+    header->s.is_free = 1;
+	pthread_mutex_unlock(&global_malloc_lock);
+}
 
 int main() {
 
